2a0cb9f0034fc2dcc66a8b95963faecc26b6d3d6
Amrish-Sharma <amrishsharma95@gmail.com>
2026-09-16 21:25:28 +0530

    updated versioncode and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.codebuzz.app.bookcric"
         minSdk = 36
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
